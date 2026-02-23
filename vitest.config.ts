a8b14adcd5eefdb32dc59647c6350538e14d9848
Michał Jaskólski <emdzej@MajBook.lan>
2026-02-23 14:47:18 +0100

    chore: use .test.ts suffix instead of .spec.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     environment: "node",
-    include: ["packages/**/src/**/*.test.ts", "packages/**/src/**/*.spec.ts"]
+    include: ["packages/**/src/**/*.test.ts"]
   }
 });
